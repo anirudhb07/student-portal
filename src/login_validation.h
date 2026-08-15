21034e292fabf1a4a3200cdb5654e5b6c01cdf9b
anirudh <bandianirudh@gmail.com>
2026-08-15 19:48:21 +0530

    Add login input validation interface

---
+++ src/login_validation.h
@@ -0,0 +1,9 @@
+#ifndef LOGIN_VALIDATION_H
+#define LOGIN_VALIDATION_H
+
+#include <string>
+
+bool validateUsername(const std::string& username);
+bool validatePassword(const std::string& password);
+
+#endif
