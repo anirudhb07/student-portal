5d5e16d86c3c568762b1494bc597f50b5859304e
anirudh <bandianirudh@gmail.com>
2026-08-16 01:15:28 +0530

    Revert "Add login activity logging"
    This reverts commit c2a88c7999e73699d5637d40beef6bfa5b30a66d.

--- src/activity_log.cpp
+++ src/activity_log.cpp
@@ -4,7 +4,3 @@
 void logPortalActivity(const std::string& activity) {
     std::cout << "Activity: " << activity << std::endl;
 }
-
-void logLoginActivity(const std::string& username) {
-    std::cout << "Login activity: " << username << std::endl;
-}
